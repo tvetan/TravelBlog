59c204fcec666813e956119fe9f80168abb7fc7c
tvetan <tvetan@gbg.bg>
2016-11-04 22:31:02 +0200

    Added home controller.

---
+++ TravelBlog/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TravelBlog.ViewModels;
+using Umbraco.Web.Models;
+using Umbraco.Web.Mvc;
+
+namespace TravelBlog.Controllers
+{
+    public class HomeController : RenderMvcController
+    {
+        public ActionResult Index(IRenderModel model)
+        {
+            var blogOverviewModel = new HomeModel(model.Content);
+            blogOverviewModel.MainTitle = "some title";
+            blogOverviewModel.MainSubTitle = "sub title";
+
+            return CurrentTemplate(blogOverviewModel);
+        }
+    }
+}
